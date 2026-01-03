5e397a79ec21f6b8db202d090d4919c42297b340
Andrew Vehlies <avehlies@gmail.com>
2026-01-03 08:26:33 -0500

    TypeDefinition updates

--- src/Phodam/Types/TypeDefinition.php
+++ src/Phodam/Types/TypeDefinition.php
@@ -13,15 +13,42 @@ use Exception;
 
 class TypeDefinition
 {
+    private string $type;
+    private ?string $name = null;
+    private bool $overriding = false;
     /** @var array<string, FieldDefinition> */
-    private array $fields;
+    private array $fields = [];
 
     /**
+     * @param string $type
+     * @param string|null $name
+     * @param bool $overriding
      * @param array<string, FieldDefinition> $fields
      */
-    public function __construct(array $fields = [])
+    public function __construct(string $type, ?string $name = null, bool $overriding = false, array $fields = [])
     {
+        $this->type = $type;
+        $this->name = $name;
+        $this->overriding = $overriding;
         $this->fields = $fields;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function isOverriding(): bool
+    {
+        return $this->overriding;
     }
 
     /**
